435a60c2dba2a672c423c463511f68d54927944b
abyss <tacan99@hotmail.com>
2014-05-10 15:28:28 +0900

    a

---
+++ jk/src/com/abyss/camera/CameraActivity.java
@@ -0,0 +1,195 @@
+package com.abyss.camera;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.abyss.R;
+
+/**
+ * Created by abyss on 2014. 5. 10..
+ */
+public class CameraActivity extends Activity {
+	private String TAG = CameraActivity.class.getSimpleName();
+	private Camera mCamera;
+	private CameraPreview mPreview;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.camera_test);
+
+		// Create an instance of Camera
+		mCamera = getCameraInstance();
+		Camera.Parameters params = mCamera.getParameters();
+		params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+		mCamera.setParameters(params);
+
+		// Create our Preview view and set it as the content of our activity.
+		mPreview = new CameraPreview(this, mCamera);
+		FrameLayout preview = (FrameLayout)findViewById(R.id.camera_preview);
+		preview.addView(mPreview);
+
+		Button captureButton = (Button)findViewById(R.id.button_capture);
+		captureButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// get an image from the camera
+				mCamera.takePicture(null, null, mPicture);
+			}
+		});
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (mCamera != null) {
+			mCamera.release();
+			mCamera = null;
+		}
+	}
+
+	public static Camera getCameraInstance() {
+		Camera c = null;
+		try {
+			c = Camera.open(); // attempt to get a Camera instance
+		} catch (Exception e) {
+			// Camera is not available (in use or does not exist)
+		}
+		return c; // returns null if camera is unavailable
+	}
+
+	private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+		@Override
+		public void onPictureTaken(byte[] data, Camera camera) {
+			Log.d(TAG, "onPictureTaken");
+		}
+	};
+
+	public void touchFocus(final int posX, final int posY) {
+		mCamera.stopFaceDetection();
+		setAutoFocusArea(mCamera, posX, posY, 128, true, new Point(mPreview.getWidth(), mPreview.getHeight()));
+		mCamera.autoFocus(mAutoFocusCallback);
+	}
+
+	private void setAutoFocusArea(Camera camera, int posX, int posY, int focusRange, boolean flag, Point point) {
+
+		if (posX < 0 || posY < 0) {
+			setArea(camera, null);
+			return;
+		}
+
+		int touchPointX;
+		int touchPointY;
+		int endFocusY;
+		int startFocusY;
+
+		if (!flag) {
+			/** Camera.setDisplayOrientation()을 이용해서 영상을 세로로 보고 있는 경우. **/
+			touchPointX = point.y >> 1;
+			touchPointY = point.x >> 1;
+
+			startFocusY = posX;
+			endFocusY = posY;
+		} else {
+			/** Camera.setDisplayOrientation()을 이용해서 영상을 가로로 보고 있는 경우. **/
+			touchPointX = point.x >> 1;
+			touchPointY = point.y >> 1;
+
+			startFocusY = posY;
+			endFocusY = point.x - posX;
+		}
+
+		float startFocusX = 1000F / (float)touchPointY;
+		float endFocusX = 1000F / (float)touchPointX;
+
+		/** 터치된 위치를 기준으로 focusing 영역으로 사용될 영역을 구한다. **/
+		startFocusX = (int)(startFocusX * (float)(startFocusY - touchPointY)) - focusRange;
+		startFocusY = (int)(endFocusX * (float)(endFocusY - touchPointX)) - focusRange;
+		endFocusX = startFocusX + focusRange;
+		endFocusY = startFocusY + focusRange;
+
+		if (startFocusX < -1000)
+			startFocusX = -1000;
+
+		if (startFocusY < -1000)
+			startFocusY = -1000;
+
+		if (endFocusX > 1000) {
+			endFocusX = 1000;
+		}
+
+		if (endFocusY > 1000) {
+			endFocusY = 1000;
+		}
+
+		/*
+		 * 주의 : Android Developer 예제 소스 처럼 ArrayList에 Camera.Area를 2개 이상 넣게 되면
+		 *          에러가 발생되는 것을 경험할 수 있을 것입니다.
+		 **/
+		Rect rect = new Rect((int)startFocusX, (int)startFocusY, (int)endFocusX, (int)endFocusY);
+		ArrayList<Camera.Area> arraylist = new ArrayList<Camera.Area>();
+		arraylist.add(new Camera.Area(rect, 1000)); // 지정된 영역을 100%의 가중치를 두겠다는 의미입니다.
+
+		setArea(camera, arraylist);
+	}
+
+	private void setArea(Camera camera, List<Camera.Area> list) {
+		boolean enableFocusModeMacro = true;
+
+		Camera.Parameters parameters;
+		parameters = camera.getParameters();
+
+		int maxNumFocusAreas = parameters.getMaxNumFocusAreas();
+		int maxNumMeteringAreas = parameters.getMaxNumMeteringAreas();
+
+		if (maxNumFocusAreas > 0) {
+			parameters.setFocusAreas(list);
+		}
+
+		if (maxNumMeteringAreas > 0) {
+			parameters.setMeteringAreas(list);
+		}
+
+		if (list == null || maxNumFocusAreas < 1 || maxNumMeteringAreas < 1) {
+			enableFocusModeMacro = false;
+		}
+
+//		if (enableFocusModeMacro == true) {
+//			/*
+//			 * FOCUS_MODE_MACRO을 사용하여 근접 촬영이 가능하도록 해야
+//			 * 지정된 Focus 영역으로 초점이 좀더 선명하게 잡히는 것을 볼 수 있습니다.
+//			 */
+//			parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_MACRO);
+//			Log.d(TAG, "focus mode macro");
+//		} else {
+//			parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+//			Log.d(TAG, "focus mode auto");
+//		}
+		camera.setParameters(parameters);
+	}
+
+	Camera.AutoFocusCallback mAutoFocusCallback = new Camera.AutoFocusCallback() {
+
+		@Override
+		public void onAutoFocus(boolean success, Camera camera) {
+			if (success) {
+				camera.cancelAutoFocus();
+			}
+
+			float focusDistances[] = new float[3];
+			camera.getParameters().getFocusDistances(focusDistances);
+
+		}
+	};
+}
---
+++ jk/src/com/abyss/camera/CameraPreview.java
@@ -0,0 +1,103 @@
+package com.abyss.camera;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by abyss on 2014. 5. 10..
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private String TAG = CameraPreview.class.getSimpleName();
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            mCamera.setPreviewDisplay(holder);
+            mCamera.setDisplayOrientation(90);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            float x = event.getX();
+            float y = event.getY();
+//		    float touchMajor = event.getTouchMajor();
+//		    float touchMinor = event.getTouchMinor();
+//
+//		    Rect touchRect = new Rect(
+//		    		(int)(x - touchMajor/2),
+//		    		(int)(y - touchMinor/2),
+//		    		(int)(x + touchMajor/2),
+//		    		(int)(y + touchMinor/2));
+
+            ((CameraActivity)getContext()).touchFocus((int) x, (int) y);
+        }
+
+
+        return true;
+    }
+
+}
